69a2bdb8eb2c0f666d63111c1845e737c007d462
Ahmed Alaa <a99_bdr@yahoo.com>
2021-04-22 23:45:16 +0200

    Update GUI

--- ProcessSchedular/Form1.Designer.cs
+++ ProcessSchedular/Form1.Designer.cs
@@ -37,51 +37,53 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.processName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.arrivingTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.burstTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Priority = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button3 = new System.Windows.Forms.Button();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 311);
+            this.label1.Location = new System.Drawing.Point(10, 383);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.Size = new System.Drawing.Size(39, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Type";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(20, 57);
+            this.textBox1.Location = new System.Drawing.Point(23, 70);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.Size = new System.Drawing.Size(140, 24);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(20, 108);
+            this.textBox2.Location = new System.Drawing.Point(23, 133);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.Size = new System.Drawing.Size(140, 24);
             this.textBox2.TabIndex = 4;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(20, 166);
+            this.textBox3.Location = new System.Drawing.Point(23, 204);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(121, 20);
+            this.textBox3.Size = new System.Drawing.Size(140, 24);
             this.textBox3.TabIndex = 5;
             // 
             // comboBox1
@@ -91,38 +93,41 @@
             "FCFS",
             "Preemptive SJF",
             "Non Preemptive SJF",
-            "Preemptive Round Robin",
-            "Non Preemptive Round Robin",
-            "Priority"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 327);
+            "Preemptive Priority",
+            "Non Preemptive Priority",
+            "Round Robin"});
+            this.comboBox1.Location = new System.Drawing.Point(14, 402);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(164, 21);
+            this.comboBox1.Size = new System.Drawing.Size(191, 24);
             this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 150);
+            this.label2.Location = new System.Drawing.Point(20, 185);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.Size = new System.Drawing.Size(73, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "Burst Time";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 92);
+            this.label3.Location = new System.Drawing.Point(20, 113);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.Size = new System.Drawing.Size(87, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Arriving Time";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 41);
+            this.label4.Location = new System.Drawing.Point(20, 50);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.Size = new System.Drawing.Size(94, 17);
             this.label4.TabIndex = 9;
             this.label4.Text = "Process Name";
             // 
@@ -138,18 +143,49 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(456, 12);
+            this.groupBox1.Location = new System.Drawing.Point(532, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(170, 293);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(198, 361);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "New Process";
             // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(23, 265);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(140, 24);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Priority";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 309);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 28);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(89, 251);
+            this.button1.Location = new System.Drawing.Point(104, 309);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(87, 28);
             this.button1.TabIndex = 10;
             this.button1.Text = "Insert";
             this.button1.UseVisualStyleBackColor = true;
@@ -164,96 +200,81 @@
             this.arrivingTime,
             this.burstTime,
             this.Priority});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 25);
+            this.dataGridView1.Location = new System.Drawing.Point(14, 31);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(438, 280);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(511, 345);
             this.dataGridView1.TabIndex = 11;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(6, 251);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Delete";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(20, 215);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(121, 20);
-            this.textBox4.TabIndex = 12;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 199);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 13);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "Priority";
             // 
             // processName
             // 
             this.processName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.processName.HeaderText = "Process Name";
+            this.processName.MinimumWidth = 6;
             this.processName.Name = "processName";
             // 
             // arrivingTime
             // 
             this.arrivingTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.arrivingTime.HeaderText = "Arriving Time";
+            this.arrivingTime.MinimumWidth = 6;
             this.arrivingTime.Name = "arrivingTime";
             // 
             // burstTime
             // 
             this.burstTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.burstTime.HeaderText = "Burst Time";
+            this.burstTime.MinimumWidth = 6;
             this.burstTime.Name = "burstTime";
             // 
             // Priority
             // 
             this.Priority.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Priority.HeaderText = "Priority";
+            this.Priority.MinimumWidth = 6;
             this.Priority.Name = "Priority";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(462, 325);
+            this.button3.Location = new System.Drawing.Point(539, 400);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(160, 23);
+            this.button3.Size = new System.Drawing.Size(187, 28);
             this.button3.TabIndex = 12;
             this.button3.Text = "Draw Gantt Chart";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // dataGridView2
+            // panel1
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(12, 363);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(617, 50);
-            this.dataGridView2.TabIndex = 13;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(14, 447);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(712, 80);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(641, 461);
-            this.Controls.Add(this.dataGridView2);
+            this.ClientSize = new System.Drawing.Size(748, 567);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Process Scheduler";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -280,8 +301,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn burstTime;
         private System.Windows.Forms.DataGridViewTextBoxColumn Priority;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.DataGridView dataGridView2;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- ProcessSchedular/Form1.cs
+++ ProcessSchedular/Form1.cs
@@ -1,29 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace test00
 {
+
+
+
+
+
+
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
-      
-        private DataTable process ;
+
+        private DataTable process;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            float arr = 0, bru = 0, pri = 0; 
+            float arr = 0, bru = 0, pri = 0;
             process = new DataTable();
-           
+
             process.Columns.Add("processName", typeof(string));
             process.Columns.Add("arrivingTime", typeof(string));
             process.Columns.Add("burstTime", typeof(string));
@@ -31,14 +33,26 @@ namespace test00
             bool canConvertArr = float.TryParse(textBox2.Text, out arr);
             bool canConvertBru = float.TryParse(textBox3.Text, out bru);
             bool canConvertPri = float.TryParse(textBox4.Text, out pri);
-            if (canConvertArr == true && canConvertBru == true && canConvertPri == true)
-                process.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+            if (comboBox1.Text != "Preemptive Priority" && comboBox1.Text != "Non Preemptive Priority")
+            {
+                if (canConvertArr == true && canConvertBru == true)
+                    process.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+                else
+                    MessageBox.Show("You have to enter decimal values for Arriving , Burst Time ", "Error Entry", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
             else
-                MessageBox.Show("You have to enter decimal values for Arriving , Burst Time ,Priority","Error Entry",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            {
+                if (canConvertArr == true && canConvertBru == true && canConvertPri == true)
+                    process.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+                else
+                    MessageBox.Show("You have to enter decimal values for Arriving , Burst Time ,Priority", "Error Entry", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
 
             DataRow newRow = process.NewRow();
-           
-            
+
+
             foreach (DataRow item in process.Rows)
             {
                 int num = dataGridView1.Rows.Add();
@@ -51,28 +65,115 @@ namespace test00
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
-            
+
+            if (dataGridView1.Rows.Count == 0) return;
             for (int i = process.Rows.Count - 1; i >= 0; i--)
             {
                 DataRow delRow = process.Rows[i];
-                if (delRow["processName"].ToString()==textBox1.Text && delRow["arrivingTime"].ToString() == textBox2.Text && delRow["burstTime"].ToString() == textBox3.Text && delRow["Priority"].ToString() == textBox4.Text) {
+                if (delRow["processName"].ToString() == textBox1.Text && delRow["arrivingTime"].ToString() == textBox2.Text && delRow["burstTime"].ToString() == textBox3.Text && delRow["Priority"].ToString() == textBox4.Text)
+                {
                     delRow.Delete();
                     dataGridView1.Rows.RemoveAt(i);
                 }
             }
-            foreach(DataGridViewRow delRow in dataGridView1.SelectedRows)
+            foreach (DataGridViewRow delRow in dataGridView1.SelectedRows)
             {
                 //DataRow delRowTable = process.Rows[delRow.Index];
                 //process.Rows[delRow.Index].Delete();
                 dataGridView1.Rows.RemoveAt(delRow.Index);
             }
-            
+
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            if (dataGridView1.Rows.Count <= 0) return;
 
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (comboBox1.Text != "Preemptive Priority" && comboBox1.Text != "Non Preemptive Priority")
+                {
+                    if (dataGridView1[0, i].Value == null || dataGridView1[1, i].Value == null || dataGridView1[2, i].Value == null)
+                    {
+                        Console.WriteLine("a");
+                        MessageBox.Show("Please input Valid Data Values");
+                        return;
+                    }
+                    
+                }
+                else
+                {
+                    if (dataGridView1[0, i].Value == null || dataGridView1[1, i].Value == null || dataGridView1[2, i].Value == null || dataGridView1[3, i].Value == null)
+                    {
+                        Console.WriteLine("b");
+                        MessageBox.Show("Please input Valid Data Values");
+                        return;
+                    }
+                    
+                }
+
+            }
+            panel1.Refresh();
+
+
+
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            int width = 40, height = 30;
+            var graphics = e.Graphics;
+            int count = dataGridView1.Rows.Count;
+
+
+            for (int i = 0; i < count; i++)
+            {
+
+                graphics.FillRectangle(new SolidBrush(Color.LightGray), new Rectangle((width * i) + 5, (panel1.Height / 3), width, height));
+                graphics.DrawRectangle(new Pen(Color.Gray), new Rectangle((width * i) + 5, (panel1.Height / 3), width, height));
+                graphics.DrawString($"{ dataGridView1[0, i].Value.ToString().ToUpper()}", new Font(FontFamily.GenericSansSerif, 13), new SolidBrush(Color.Black), new Point((width * i) + 15, (panel1.Height / 3) + 5));
+                graphics.DrawString($"{ dataGridView1[1, i].Value.ToString()}", new Font(FontFamily.GenericSerif, 8), new SolidBrush(Color.White), new Point((width * i) + 5, (panel1.Height / 3) + height));
+
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+
+            if (comboBox1.Text != "Preemptive Priority" && comboBox1.Text != "Non Preemptive Priority")
+                {
+                    textBox4.Enabled = false;
+                    dataGridView1.Columns[3].ReadOnly = true;
+
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                        dataGridView1.Rows[i].Cells[3].Value = "";
+                    textBox4.Text = "";
+                }
+                else
+                {
+                    textBox4.Enabled = true;
+                    dataGridView1.Columns[3].ReadOnly = false;
+                }
+            
+            
+           
+        }
+
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
         }
     }
 }
