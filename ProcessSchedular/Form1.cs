49514958eb495291c5ec6f6fd87ef695bb32fb74
Abdurrahman Eshibiny <a.eshibiny@gmail.com>
2021-04-26 01:52:42 +0200

    Added my code for roundrobin (not complete yet) and made columns for the turnaround and waiting times

--- ProcessSchedular/Form1.cs
+++ ProcessSchedular/Form1.cs
@@ -28,11 +28,13 @@ namespace test00
             process.Columns.Add("arrivingTime", typeof(string));
             process.Columns.Add("burstTime", typeof(string));
             process.Columns.Add("Priority", typeof(string));
+            process.Columns.Add("waitingTime", typeof(string));
+            process.Columns.Add("turnaroundTime", typeof(string));
             bool canConvertArr = float.TryParse(textBox2.Text, out arr);
             bool canConvertBru = float.TryParse(textBox3.Text, out bru);
             bool canConvertPri = float.TryParse(textBox4.Text, out pri);
             if (canConvertArr == true && canConvertBru == true && canConvertPri == true)
-                process.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+                process.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, "0", "0");
             else
                 MessageBox.Show("You have to enter decimal values for Arriving , Burst Time ,Priority","Error Entry",MessageBoxButtons.OK,MessageBoxIcon.Error);
 
@@ -73,6 +75,47 @@ namespace test00
         private void button3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        static void roundrobin(DataTable processes, int quantum)
+        {
+            //TODO implement return of average waiting and turnaround times
+            int time = 0;
+            int done = 0;
+            int processes_no = processes.Rows.Count;
+            bool madeProgress = false;
+
+            while (done != processes_no)
+            {
+                madeProgress = false;
+                for (int i = 0; i < processes_no; i++)
+                {
+                    DataRow process = processes.Rows[i];
+
+                    if (Convert.ToInt32(process["arrivingTime"]) <= time && Convert.ToInt32(process["burstTime"]) > 0)
+                    {
+                        if (Convert.ToInt32(process["burstTime"]) <= quantum)
+                        {
+                            Console.WriteLine(process["processName"] + " " + Convert.ToString(time) + " " + Convert.ToString(time + Convert.ToInt32(process["burstTime"])));
+                            time += Convert.ToInt32(process["burstTime"]);
+                            process["burstTime"] ="0";
+                            process["turnaroundTime"] = Convert.ToString(time - Convert.ToInt32(process["arrivingTime"]));
+                            process["waitingTime"] = Convert.ToString(Convert.ToInt32(process["turnaroundTime"]) + Convert.ToInt32(process["waitingTime"]));
+                            done += 1;
+                            madeProgress = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine(process["processName"] + " " + Convert.ToString(time) + " " + Convert.ToString(time + quantum));
+                            time += quantum;
+                            process["burstTime"] = Convert.ToString(Convert.ToInt32(process["burstTime"]) - quantum);
+                            process["waitingTime"] = Convert.ToString(Convert.ToInt32(process["waitingTime"]) - quantum);
+                            madeProgress = true;
+                        }
+                    }
+                }
+                time = (madeProgress) ? time : time + quantum;
+            }
         }
     }
 }
