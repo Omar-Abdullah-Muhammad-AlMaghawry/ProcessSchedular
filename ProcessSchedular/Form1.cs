0544de7635dc7ea597037ddd4ce2475a7c2ee194
Ahmed Alaa <a99_bdr@yahoo.com>
2021-04-28 04:22:42 +0200

    SJF

--- ProcessSchedular/Form1.cs
+++ ProcessSchedular/Form1.cs
@@ -39,6 +39,12 @@ namespace test00
             bool canConvertArr = float.TryParse(textBox2.Text, out arr);
             bool canConvertBru = float.TryParse(textBox3.Text, out bru);
             bool canConvertPri = float.TryParse(textBox4.Text, out pri);
+            if (!textBox1.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("You have to enter decimal values for Process name ", "Error Entry", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
             if (comboBox1.Text != "Preemptive Priority" && comboBox1.Text != "Non Preemptive Priority")
             {
                 if (canConvertArr == true && canConvertBru == true)
@@ -129,6 +135,27 @@ namespace test00
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+
+            int no_processes = dataGridView1.Rows.Count;
+            Console.WriteLine(Convert.ToInt32(no_processes));
+            int[,] array = new int[no_processes, 7];
+            //read from table and put in a private array
+            
+            for (int i = 0; i < no_processes; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (j == 0)
+                        array[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+                    else if (j == 1)
+                        array[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+                    else if (j == 2)
+                        array[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[2].Value);
+
+
+                }
+            }
+
             int width = 40, height = 30;
             var graphics = e.Graphics;
             int count = dataGridView1.Rows.Count;
@@ -144,6 +171,29 @@ namespace test00
                 {
                     FCFS(width, height, graphics, ref sum, ref totalWaiting, i);
 
+                }
+                else if (comboBox1.Text == "Non Preemptive SJF")
+                {
+                    if (i == 0)
+                    {
+                        non_premitiveSort(array);
+
+                        graphics.DrawString($"{ array[i, 0].ToString().ToUpper()}", new Font(FontFamily.GenericSansSerif, 13), new SolidBrush(Color.Black), new Point((width * i) + 15, (panel1.Height / 3) + 5));
+
+
+                        graphics.DrawString($"{array[i, 6]}", new Font(FontFamily.GenericSerif, 8), new SolidBrush(Color.White), new Point((width * i) + 5, (panel1.Height / 3) + height));
+                        graphics.DrawString($"{array[i, 3]}", new Font(FontFamily.GenericSerif, 8), new SolidBrush(Color.Black), new Point((width * i) + width - 10, (panel1.Height / 3) + height));
+
+
+                    }
+                    else
+                    {
+                        graphics.DrawString($"{ array[i, 0].ToString().ToUpper()}", new Font(FontFamily.GenericSansSerif, 13), new SolidBrush(Color.Black), new Point((width * i) + 15, (panel1.Height / 3) + 5));
+
+
+                        graphics.DrawString($"{array[i, 6]}", new Font(FontFamily.GenericSerif, 8), new SolidBrush(Color.White), new Point((width * i) + 5, (panel1.Height / 3) + height));
+                        graphics.DrawString($"{array[i, 3]}", new Font(FontFamily.GenericSerif, 8), new SolidBrush(Color.Black), new Point((width * i) + width - 10, (panel1.Height / 3) + height));
+                    }
                 }
                 else
                 {
@@ -217,7 +267,150 @@ namespace test00
             textBox4.Enabled = false;
         }
 
-        
-        
+        static void Swap(ref int x, ref int y)
+        {
+            int tempswap = x;
+            x = y;
+            y = tempswap;
+        }
+        private void non_premitiveSort(int[,] array)
+        {
+            int no_processes = dataGridView1.Rows.Count;
+
+            //sort according to the arrival time
+            for (int i = 0; i < no_processes; i++)
+            {
+                for (int j = 0; j < no_processes - i - 1; j++)
+                {
+                    if (array[j, 1] > array[j + 1, 1])
+                    {
+                        for (int k = 0; k < 4; k++)
+                        {
+                            Swap(ref (array[j, k]), ref (array[j + 1, k]));
+                        }
+                    }
+                }
+            }
+
+         
+            //sort according to the butst time
+            int temp;
+            bool flag = false;
+            int val = 0;
+            for (int i = 0; i < no_processes - 1; i++)
+            {
+                if (array[i, 1] == array[i + 1, 1] && array[i, 1] == 0)
+                {
+                    flag = true;
+                }
+            }
+            if (flag == false)
+            {
+                array[0, 3] = array[0, 1] + array[0, 2];
+                for (int i = 1; i < no_processes; i++)
+                {
+
+                    temp = array[i - 1, 3];
+                    int low = array[i, 2];//brust time
+                    for (int j = i; j < no_processes; j++) //to loop rows from 1 to num-1 to find the next Shortest job
+                    {
+                        if (temp >= array[j, 1] && low >= array[j, 2]) //arrivaltime smaller than completionTime of cuurent process
+                        {
+                            low = array[j, 2];
+                            val = j;
+                        }
+                    }
+                    array[val, 3] = temp + array[val, 2];
+                    for (int k = 0; k < 4; k++)
+                    {
+                        Swap(ref (array[val, k]), ref (array[i, k])); //fuction to swap row if needed
+                    }
+                }
+
+            }
+
+            else
+            {
+
+                for (int i = 0; i < no_processes; i++)
+                {
+                    if (i == 0)
+                    {
+                        temp = array[i, 3];
+                        decimal low = array[i, 2];//brust time
+                        for (int j = i; j < no_processes; j++) //to loop rows from 1 to num-1 to find the next Shortest job
+                        {
+                            if (temp >= array[j, 1] && low >= array[j, 2]) //arrivaltime smaller than completionTime of cuurent process
+                            {
+                                low = array[j, 2];
+                                val = j;
+                            }
+                        }
+                        array[val, 3] = temp + array[val, 2];
+                        for (int k = 0; k < 4; k++)
+                        {
+                            Swap(ref (array[val, k]), ref (array[i, k])); //fuction to swap row if needed
+                        }
+                    }
+
+
+                    else
+                    {
+                        temp = array[i - 1, 3];
+                        decimal low = array[i, 2];//brust time
+                        for (int j = i; j < no_processes; j++) //to loop rows from 1 to num-1 to find the next Shortest job
+                        {
+                            if (temp >= array[j, 1] && low >= array[j, 2]) //arrivaltime smaller than completionTime of cuurent process
+                            {
+                                low = array[j, 2];
+                                val = j;
+                            }
+                        }
+                        array[val, 3] = temp + array[val, 2];
+                        for (int k = 0; k < 4; k++)
+                        {
+                            Swap(ref (array[val, k]), ref (array[i, k])); //fuction to swap row if needed
+                        }
+                    }
+                }
+            }
+            //put the coloum 4 for the turnaround
+            for (int i = 0; i < no_processes; i++)
+            {
+                array[i, 4] = array[i, 3] - array[i, 1];
+            }
+            //put coloumn 5 for the waiting time
+            for (int i = 0; i < no_processes; i++)
+            {
+                array[i, 5] = array[i, 4] - array[i, 2];
+            }
+            //put coloumn 6 for the start time
+            for (int i = 0; i < no_processes; i++)
+            {
+                array[i, 6] = array[i, 3] - array[i, 2];
+            }
+
+            //calculate total waiting time
+            decimal waiting_time = 0;
+            for (int i = 0; i < no_processes; i++)
+            {
+                waiting_time = waiting_time + array[i, 5];
+            }
+
+            for (int i = 0; i < no_processes; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                    Console.WriteLine(array[i, j]);
+            }
+
+
+            float average_waiting_time = (float)waiting_time / no_processes;
+            Console.WriteLine(average_waiting_time);
+            textBox5.Text = Convert.ToString(average_waiting_time);
+        }
+
+
+
     }
+
 }
